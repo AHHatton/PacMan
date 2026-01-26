e02ca9eecc334a514624704a596e56676adeb8d5
FessorFace <amanda.h.hatton@gmail.com>
2026-01-26 14:44:25 -0900

    updated base name to match change in repo name.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PacMan/",
+  base: "/WordleSchmurdle/",
   plugins: [
     vue(),
     vueDevTools(),
